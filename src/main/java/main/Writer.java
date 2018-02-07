2d084f593aaf5d9d6992807c3e9b031f421a66b0
Etelstan <etelstan@bk.ru>
2018-02-07 23:03:09 +0500

    part two

--- src/main/java/main/Writer.java
+++ src/main/java/main/Writer.java
@@ -2,6 +2,7 @@ package main;
 
 public class Writer {
   private volatile String res = "";   // строка с данными из потоков
+  private volatile boolean current;   // переключатель потоков
 
   public String getRes() {
     return res;
@@ -14,5 +15,25 @@ public class Writer {
   private synchronized void add(int num) {
     res += num;
     System.out.println(res);
+  }
+
+  public void startThread(String name, int value, boolean active) {
+    Thread thread = new Thread(() -> {
+      while (true) {
+        if (active ^ current) {
+          for (int i = 0; i < 10; i++) {
+            add(value);
+            try {
+              Thread.sleep(20);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+          current = !current;
+        }
+      }
+    });
+    thread.setName(name);
+    thread.start();
   }
 }
